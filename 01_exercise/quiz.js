5afa450267e3c268e8675196ed21a7a8a8a0ba25
Elena-Sea <o.smetyuk@gmail.com>
2022-10-25 09:53:01 +0200

    Adds the skeleton of quiz array

---
+++ 01_exercise/quiz.js
@@ -0,0 +1,19 @@
+const qiestions = [
+    {
+        question: 'How old am I?',
+        wrongAnswer: '3',
+        wrongAnswer: '2',
+        wrongAnswer: '5',
+        wrongAnswer: '34',
+        RightAnswer: '9',
+    }, 
+    {
+        question: 'How old am I?',
+        wrongAnswer: '3',
+        wrongAnswer: '2',
+        wrongAnswer: '5',
+        wrongAnswer: '34',
+        RightAnswer: '9',
+    },
+
+]
