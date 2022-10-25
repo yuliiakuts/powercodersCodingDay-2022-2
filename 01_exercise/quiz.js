b9515b404527b8e973104b1ab0c9c108cf0d4e61
Arwa <arwa.alhussein@powercoders.org>
2022-10-25 10:04:32 +0200

    Questions & Answers

--- 01_exercise/quiz.js
+++ 01_exercise/quiz.js
@@ -1,19 +1,41 @@
-const qiestions = [
+const questions = [
     {
-        question: 'How old am I?',
-        wrongAnswer: '3',
-        wrongAnswer: '2',
-        wrongAnswer: '5',
-        wrongAnswer: '34',
-        RightAnswer: '9',
+        question: "What's the skeleton structure on a website?",
+        wrongAnswer: 'JavaScript',
+        wrongAnswer: 'Python',
+        wrongAnswer: 'CSS',
+        wrongAnswer: 'React',
+        RightAnswer: 'HTML',
     }, 
     {
-        question: 'How old am I?',
-        wrongAnswer: '3',
-        wrongAnswer: '2',
-        wrongAnswer: '5',
-        wrongAnswer: '34',
-        RightAnswer: '9',
+        question: 'What to use in JAVAscript to make any website interactive',
+        wrongAnswer: 'Functions',
+        wrongAnswer: 'Looping',
+        wrongAnswer: 'Bubbling',
+        wrongAnswer: 'Conditions',
+        RightAnswer: 'AddEventListener',
+    },
+    {
+        question: 'What keyword is used for creating an instance of an object?',
+        wrongAnswer: 'make',
+        wrongAnswer: 'var',
+        wrongAnswer: 'inst',
+        rightAnswer: 'new'
+        },
+    {
+        question: 'What is the output? const age = 26; const beverage = age >= 21 ? "Beer" : "Juice"; console.log(beverage);' ,
+        wrongAnswer: 'Error',
+        wrongAnswer: 'Juice',
+        rightAnswer: 'Beer',
+        wrongAnswer: 'Water',
+    },
+    {
+        question: 'Learing how to programme means:',
+        wrongAnswer: 'how to prepare an agenda',
+        wrongAnswer: 'how to solve mathematical problem',
+        wrongAnswer: 'how to overcome stress',
+        wrongAnswer: 'how to communicate with aliens',
+        RightAnswer: 'how to solve problems using code',
     },
 
 ]
